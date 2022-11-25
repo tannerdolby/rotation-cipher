626df7047465807e764ff34eeb3f3ad26e5f9cde
Tanner <tannercdolby@gmail.com>
2022-11-24 20:20:53 -0800

    add start of decrypt tests

---
+++ test/decrypt.test.js
@@ -0,0 +1,13 @@
+const { decrypt } = require('../src/index')
+
+describe('decrypt cipher tests', () => {
+
+    test('decrypt a uniform rotation string', () => {
+        expect(decrypt('kreevi', 17)).toBe('tanner')
+        expect(decrypt('gnaare')).toBe('tanner')
+    })
+
+    test('decrypt a custom rotation', () => {
+        expect(decrypt('foo', null, [3, 4, 5])).toBe('ckj')
+    })
+})
